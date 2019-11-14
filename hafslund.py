3e85f19071661dcfaf2bc6d6b216d770a0fffa42
Paal Braathen <paalbra@gmail.com>
2019-11-14 11:50:58 +0100

    Fix wrong api in get_consumption

--- hafslund.py
+++ hafslund.py
@@ -51,7 +51,7 @@ class HafslundAPI():
 
     def get_consumption(self, meter_point_id, start_date, end_data, resolution):
         # resolution: hourly, week-stats
-        url = f"https://api.linkapp.no/api/consumption/{meter_point_id}/{start_date}/{end_data}/{resolution}"
+        url = f"https://api2.linkapp.no/api/consumption/{meter_point_id}/{start_date}/{end_data}/{resolution}"
 
         return self.do_request(url, auth=True)
 
